90f78fc83d56535e68af0b855f4288065b0f0a52
Zhengheng Li <zhenghenge@gmail.com>
2021-05-06 09:52:18 +0800

    enable NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {})],
     providers: [],
     bootstrap: [AppComponent],
 })
