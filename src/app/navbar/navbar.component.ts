fecebb895f88623123170284a871f099e86aa18e
Zhengheng Li <zhenghenge@gmail.com>
2021-05-08 09:43:32 +0800

    add validator for hours input

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -9,21 +9,23 @@ import { Subscription } from 'rxjs';
 })
 export class NavbarComponent implements OnInit, OnDestroy {
     date = new FormControl(null, { updateOn: 'blur' });
-    hours = new FormControl(null, { updateOn: 'blur' });
+    hours = new FormControl(null, { updateOn: 'blur', validators: [Validators.min(1), Validators.max(24)] });
     subs = new Subscription();
 
     constructor() {}
 
     ngOnInit(): void {
         this.subs.add(
-            this.date.valueChanges.subscribe((v) => {
-                console.log(v);
+            this.date.valueChanges.subscribe(() => {
+                console.log(this.date.value);
+                console.log(this.date.valid);
                 this.hours.reset(null, { emitEvent: false });
             })
         );
         this.subs.add(
-            this.hours.valueChanges.subscribe((v) => {
-                console.log(v);
+            this.hours.valueChanges.subscribe(() => {
+                console.log(this.hours.value);
+                console.log(this.hours.valid);
                 this.date.reset(null, { emitEvent: false });
             })
         );
