203d18a23aaa32caeeef89c63c048f5c2a4c997b
Zhengheng Li <zhenghenge@gmail.com>
2021-05-10 09:45:35 +0800

    add lb4 models

---
+++ src/app/state-management/models/index.ts
@@ -0,0 +1,40 @@
+export interface NodeCpuLoad {
+    id?: number;
+    time: number; // unix timestamp in millisecond
+    node: string;
+    user: number;
+    system: number;
+    idle: number;
+    iowait: number;
+    irq: number;
+    softirq: number;
+    steal: number;
+    guest: number;
+    memory: number;
+}
+
+export interface NodeGpuLoad {
+    id?: number;
+    time: number; // unix timestamp in millisecond
+    node: string;
+    busid: string;
+    gpu: number;
+    mem: number;
+}
+
+export interface UserCpuMem {
+    id?: number;
+    time: number; // unix timestamp in millisecond
+    node: string;
+    user: string;
+    cpu: number;
+    mem: number;
+}
+
+export interface TimeRangeFilter {
+    where: {
+        time: {
+            between: [number, number]; // unix timestamp in millisecond
+        };
+    };
+}
