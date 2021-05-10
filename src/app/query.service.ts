cdeb01d63ee7389c2267215eead109744264c6e1
Zhengheng Li <zhenghenge@gmail.com>
2021-05-10 14:21:18 +0800

    query.service.ts: fetch api server url

--- src/app/query.service.ts
+++ src/app/query.service.ts
@@ -4,7 +4,21 @@ import { Injectable } from '@angular/core';
     providedIn: 'root',
 })
 export class QueryService {
+    private apiServerUrl: string | undefined;
+
     constructor() {
-        console.log('service init');
+        const configUrl: string = 'assets/config.json';
+        (async () => {
+            const response = await fetch(configUrl);
+            if (!response.ok) {
+                throw new Error(`cannot fetch config data from ${configUrl}`);
+            }
+            const configData = await response.json();
+            this.apiServerUrl = configData.api_server_url;
+            if (!this.apiServerUrl) {
+                throw new Error(`failed to get api server url in config ${configUrl}`);
+            }
+            console.log('apiServerUrl:', this.apiServerUrl);
+        })();
     }
 }
