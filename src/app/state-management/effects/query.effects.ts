b903fe4ddea20855948263d95c8df86ada18347d
Zhengheng Li <zhenghenge@gmail.com>
2021-05-11 16:49:58 +0800

    query.effects.ts

--- src/app/state-management/effects/query.effects.ts
+++ src/app/state-management/effects/query.effects.ts
@@ -5,6 +5,7 @@ import { exhaustMap, map, catchError } from 'rxjs/operators';
 import { QueryService } from '../../query.service';
 import {
     lastHours,
+    selectDate,
     nodeCpuLoadSuccess,
     nodeCpuLoadFailure,
     nodeGpuLoadSuccess,
@@ -31,6 +32,20 @@ export class QueryEffects {
         )
     );
 
+    getNodeCpuLoadDate = createEffect(() =>
+        this.actions.pipe(
+            ofType(selectDate),
+            exhaustMap((action) => {
+                const minTime = action.date.getTime();
+                const maxTime = minTime + 24 * 3600 * 1000;
+                return this.query.getNodeCpuLoad(minTime, maxTime).pipe(
+                    map((data) => nodeCpuLoadSuccess({ data })),
+                    catchError((error) => of(nodeCpuLoadFailure({ error })))
+                );
+            })
+        )
+    );
+
     getNodeGpuLoadHour = createEffect(() =>
         this.actions.pipe(
             ofType(lastHours),
@@ -45,12 +60,40 @@ export class QueryEffects {
         )
     );
 
-    getUserCpuMem = createEffect(() =>
+    getNodeGpuLoadDate = createEffect(() =>
+        this.actions.pipe(
+            ofType(selectDate),
+            exhaustMap((action) => {
+                const minTime = action.date.getTime();
+                const maxTime = minTime + 24 * 3600 * 1000;
+                return this.query.getNodeGpuLoad(minTime, maxTime).pipe(
+                    map((data) => nodeGpuLoadSuccess({ data })),
+                    catchError((error) => of(nodeGpuLoadFailure({ error })))
+                );
+            })
+        )
+    );
+
+    getUserCpuMemHour = createEffect(() =>
         this.actions.pipe(
             ofType(lastHours),
             exhaustMap((action) => {
                 const maxTime = new Date().getTime();
                 const minTime = maxTime - action.hours * 3600 * 1000;
+                return this.query.getUserCpuMem(minTime, maxTime).pipe(
+                    map((data) => userCpuMemSuccess({ data })),
+                    catchError((error) => of(userCpuMemFailure({ error })))
+                );
+            })
+        )
+    );
+
+    getUserCpuMemDate = createEffect(() =>
+        this.actions.pipe(
+            ofType(selectDate),
+            exhaustMap((action) => {
+                const minTime = action.date.getTime();
+                const maxTime = minTime + 24 * 3600 * 1000;
                 return this.query.getUserCpuMem(minTime, maxTime).pipe(
                     map((data) => userCpuMemSuccess({ data })),
                     catchError((error) => of(userCpuMemFailure({ error })))
