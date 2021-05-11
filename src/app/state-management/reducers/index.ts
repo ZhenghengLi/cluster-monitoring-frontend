b9b3c223b6c8ff81a35f7b59fcc4ef0ecb786186
Zhengheng Li <zhenghenge@gmail.com>
2021-05-11 17:01:10 +0800

    reducers

--- src/app/state-management/reducers/index.ts
+++ src/app/state-management/reducers/index.ts
@@ -1,12 +1,33 @@
 import { createReducer, on, ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
 import { State, initialCpuOverview, initialGpuOverview, initialTopUsers } from '../state';
+import { nodeCpuLoadSuccess, nodeGpuLoadSuccess, userCpuMemSuccess } from '../actions';
 
-const cpuOverviewReducer = createReducer(initialCpuOverview);
+const cpuOverviewReducer = createReducer(
+    initialCpuOverview,
+    on(nodeCpuLoadSuccess, (state, action) => {
+        console.log(action);
+        console.log(state);
+        return state;
+    })
+);
 
-const gpuOverviewReducer = createReducer(initialGpuOverview);
+const gpuOverviewReducer = createReducer(
+    initialGpuOverview,
+    on(nodeGpuLoadSuccess, (state, action) => {
+        console.log(action);
+        console.log(state);
+        return state;
+    })
+);
 
-const topUsersReducer = createReducer(initialTopUsers);
+const topUsersReducer = createReducer(
+    initialTopUsers,
+    on(userCpuMemSuccess, (state, action) => {
+        console.log(action);
+        return state;
+    })
+);
 
 export const reducers: ActionReducerMap<State> = {
     cpuOverview: cpuOverviewReducer,
