a8b7b0dfe33a850cb1b1984f4c8eba3804fa0a9a
Zhengheng Li <zhenghenge@gmail.com>
2021-05-12 19:06:55 +0800

    add real name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ChartLine, ChartUsers } from './state-management/models';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Store } from '@ngrx/store';
 import { State } from './state-management/state';
 
@@ -18,18 +19,21 @@ export class AppComponent implements OnInit {
 
     topUsersChart: Observable<ChartUsers[]>;
     topUsersColumns: string[] = ['name', 'utilization', 'memory'];
+    nameMap = {};
 
     trackChart(index: number, item: ChartUsers): string {
         return item.name;
     }
 
-    constructor(private store: Store<State>) {
+    constructor(private store: Store<State>, private http: HttpClient) {
         this.cpuUtilChart = this.store.select('cpuOverview', 'utilization');
         this.cpuMemChart = this.store.select('cpuOverview', 'memory');
         this.gpuUtilChart = this.store.select('gpuOverview', 'utilization');
         this.gpuMemChart = this.store.select('gpuOverview', 'memory');
-
         this.topUsersChart = this.store.select('topUsers');
+        // load user map
+        const nameMapUrl = 'assets/name-map.json';
+        this.http.get<{ [name: string]: string }>(nameMapUrl).subscribe((data) => (this.nameMap = data));
     }
 
     ngOnInit(): void {}
